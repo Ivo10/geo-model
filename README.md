cef85a9a760d3e1db302ace88f341b99773c0cc9
Ivo10 <70121270+Ivo10@users.noreply.github.com>
2024-08-04 15:33:42 +0800

    bug解决

--- README.md
+++ README.md
@@ -1,40 +1,11 @@
-1. 范围[10, 10, 5]；
+# 图神经网络构造建模
+
+1. 范围[[0, 10], [0, 10], [0, 5]]；
 
 2. 输入顶点为258个，前8个顶点为六面体的顶点，后250个为随机生成的顶点；
 
-3. 生成顶点信息：
 
-   ```
-   Statistics:
-   
-     Input points: 50
-     Input facets: 12
-     Input segments: 12
-     Input holes: 0
-     Input regions: 0
-   
-     Mesh points: 2141
-     Mesh tetrahedra: 9921
-     Mesh faces: 20838
-     Mesh faces on exterior boundary: 1992
-     Mesh faces on input facets: 1992
-     Mesh edges on input segments: 153
-     Steiner points on input facets:  849
-     Steiner points on input segments:  141
-     Steiner points inside domain: 1101
-   ```
-   
-4. 由于z的取值范围为[0,5]，z坐标和地层$f_v$的映射关系定义为：
-
-   | z坐标  | 地层$f_v$ |
-   | ------ | --------- |
-   | [0, 1) | -0.66     |
-   | [1, 2) | -0.33     |
-   | [2, 3) | 0         |
-   | [3, 4) | 0.33      |
-   | [4, 5] | 0.66      |
-
-5. 定义图卷积网络信息
+3. 定义图卷积网络信息
 
    | 层数      | 信息    |
    | --------- | ------- |
@@ -42,5 +13,3 @@
    | 图卷积层2 | 16->64  |
    | 图卷积层3 | 64->128 |
    | 全连接层  | 128->1  |
-
-   ![image-20240731145616389](C:\Users\Xiong Wei\AppData\Roaming\Typora\typora-user-images\image-20240731145616389.png)
--- loss/ComputeGradient.py
+++ loss/ComputeGradient.py
@@ -1,13 +1,12 @@
 import torch
 
 
-# 定义函数来计算梯度
 def compute_gradients(x, z, matrix):
     n = x.shape[0]
     gradients = []
 
     for i in range(n):
-        # 找到与i点相连的点
+        # 找到与节点i的邻居index
         connected_indices = matrix[i].nonzero(as_tuple=True)[0]
 
         # 没有连接的点，则梯度为0
@@ -15,17 +14,14 @@ def compute_gradients(x, z, matrix):
             gradients.append(torch.zeros(3))
             continue
 
-        # 构建P_v和S_v
         P_v = x[connected_indices] - x[i]
         S_v = z[connected_indices] - z[i]
 
         # 计算梯度
         P_v_t = P_v.t()
         try:
-            # 求解正规方程
             gradient = torch.linalg.solve(P_v_t @ P_v, P_v_t @ S_v)
         except RuntimeError:
-            # 如果P_v^T P_v是奇异的，使用伪逆
             gradient = torch.linalg.pinv(P_v_t @ P_v) @ P_v_t @ S_v
 
         gradients.append(gradient.squeeze())
@@ -37,5 +33,3 @@ if __name__ == '__main__':
     x = torch.tensor([[0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]], dtype=torch.float32)
     matrix = torch.tensor([[0, 1, 1], [1, 0, 0], [1, 0, 0]], dtype=torch.float32)
     z = torch.tensor([[0.0], [1.0], [2.0]], dtype=torch.float32)
-
-    print(compute_gradients(x, z, matrix).shape)
